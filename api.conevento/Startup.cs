7b4b5a62306fd592b12ca6fefbb03ce4dbfaa8b5
Rodrigo A <linelsk@hotmail.com>
2022-01-31 15:57:47 -0600

    Ajustes servicio calendar

--- api.conevento/Startup.cs
+++ api.conevento/Startup.cs
@@ -157,7 +157,7 @@ namespace api.conevento
                 c.SwaggerEndpoint("../swagger/v1/swagger.json", "Conevento API V1");
 #else
               
-                c.SwaggerEndpoint("/back/api_conevento/swagger/v1/swagger.json", "Web API V1");
+                c.SwaggerEndpoint("/API-conevento/swagger/v1/swagger.json", "Web API V1");
 #endif
             });
             app.UseEndpoints(endpoints =>
